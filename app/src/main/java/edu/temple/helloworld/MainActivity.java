2b932adfc4c21d632101609b25758eb59f6786fa
jdw <jdw@notarealaddress.com>
2020-09-17 02:48:21 -0400

    Added functionality to the button to change the text in the textView field.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,29 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button myButton;
+    TextView thisText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        thisText = findViewById(R.id.textView1);
+        myButton = findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this, "The button has been clicked.", Toast.LENGTH_SHORT).show();
+                thisText.setText("Thanks for clicking that button!");
+            }
+        });
     }
 }
